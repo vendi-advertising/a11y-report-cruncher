d313f6ebf1e1ee66eca1fc37b3fbec847c04fab7
Chris Haas <chris@vendiadvertising.com>
2019-02-08 14:32:35 -0600

    Added YUML mapping

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Onurb\Bundle\YumlBundle\OnurbYumlBundle::class => ['all' => true],
 ];
