338971ceb09e8da832472491af2d9c11f3a45fb7
Chris Haas <chris@vendiadvertising.com>
2019-02-08 17:09:24 -0600

    Add edit support to user
    Closes #1

--- src/Command/MakeUserCommand.php
+++ src/Command/MakeUserCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -19,6 +20,9 @@ class MakeUserCommand extends Command
 
     private $passwordEncoder;
     private $entityManager;
+
+    private $input;
+    private $output;
 
     public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
     {
@@ -36,35 +40,28 @@ class MakeUserCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        //We need these stored for the helpers
+        $this->input = $input;
+        $this->output = $output;
+
         $io = new SymfonyStyle($input, $output);
         $helper = $this->getHelper('question');
 
         $question = (new Question('What is the user\'s email address? '))
+
+            //Trim extra whitespace
             ->setNormalizer(
                 function ($value) {
                     return $value ? trim($value) : '';
                 }
             )
+
+            //Force a valid email address
             ->setValidator(
                 function ($email) {
                     if (!is_string($email) || !filter_var($email, \FILTER_VALIDATE_EMAIL)) {
                         throw new \RuntimeException('Please enter a valid email address');
                     }
-
-                    $previous_user = $this
-                                        ->entityManager
-                                        ->getRepository(User::class)
-                                        ->findOneBy(
-                                            array(
-                                                'email' => $email,
-                                            )
-                                        )
-                    ;
-
-                    if($previous_user){
-                        throw new \RuntimeException('A user with this email address already exists');
-                    }
-
 
                     return $email;
                 }
@@ -73,16 +70,122 @@ class MakeUserCommand extends Command
 
         $email = $helper->ask($input, $output, $question);
 
+        //Try getting the current user
+        $user = $this->get_user_by_email($email);
 
+        $is_new_user = is_null($user);
+        $is_dirty = false;
+        if($is_new_user){
+            $user = new User();
+            $user->setEmail($email);
+            $is_dirty = true;
+        }
+
+        $methods = [
+            'perform_email_change_question',
+            'perform_password_question',
+        ];
+
+        foreach($methods as $method){
+            if($this->$method($user, $is_new_user)){
+                $is_dirty = true;
+            }
+        }
+
+        if(!$is_dirty){
+            $io->note('No changes to the user\'s information were detected so no work was done');
+            return;
+        }
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        if($is_new_user){
+            $io->success("Successfully created new user {$email}");
+        }else{
+            $io->success("Successfully updated user {$email}");
+        }
+    }
+
+    protected function get_user_by_email(string $email) : ?User
+    {
+        return $this
+                    ->entityManager
+                    ->getRepository(User::class)
+                    ->findOneBy(
+                        array(
+                            'email' => $email,
+                        )
+                    )
+        ;
+
+    }
+
+    protected function perform_email_change_question(User $user, bool $is_new_user) : bool
+    {
+        if($is_new_user){
+            //Don't offer for new users, that's just stupid
+            return false;
+        }
+
+        while(true){
+
+            if(!$this->ask_yes_or_no_question('Would you like to change the user\'s email? ', false)){
+                return false;
+            }
+
+            $helper = $this->getHelper('question');
+            $question = (new Question('What is the user\'s new email address? '))
+
+                //Trim extra whitespace
+                ->setNormalizer(
+                    function ($value) {
+                        return $value ? trim($value) : '';
+                    }
+                )
+
+                //Force a valid email address
+                ->setValidator(
+                    function ($email) {
+                        if (!is_string($email) || !filter_var($email, \FILTER_VALIDATE_EMAIL)) {
+                            throw new \RuntimeException('Please enter a valid email address');
+                        }
+
+                        if($this->get_user_by_email($email)){
+                            throw new \RuntimeException('That email address is already in use');
+                        }
+
+                        return $email;
+                    }
+                )
+            ;
+
+            $email = $helper->ask($this->input, $this->output, $question);
+            $user->setEmail($email);
+
+            return true;
+
+        }
+
+
+    }
+
+    protected function perform_password_question(User $user, bool $is_new_user) : bool
+    {
+        if(!$is_new_user){
+            if(!$this->ask_yes_or_no_question('Would you like to change the user\'s password? ', false)){
+                return false;
+            }
+        }
+
+        //Setup the question
         $question = new Question('What is the user\'s new password? ');
         $question->setHidden(true);
         $question->setHiddenFallback(false);
-        $password = $helper->ask($input, $output, $question);
 
-        $user = new User();
-        $user->setEmail($email);
+        //Ask it
+        $password = $helper->ask($this->input, $this->output, $question);
 
-        // encode the plain password
         $user->setPassword(
             $this
                 ->passwordEncoder
@@ -92,9 +195,13 @@ class MakeUserCommand extends Command
                 )
         );
 
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        return true;
+    }
 
-        $io->success("Successfully create new user {$email}");
+    protected function ask_yes_or_no_question(string $question, bool $default_value) : bool
+    {
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion( $question, $default_value );
+        return $helper->ask($this->input, $this->output, $question);
     }
 }
