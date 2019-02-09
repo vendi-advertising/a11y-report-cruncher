f14a3be9c96987f8f52c588235bb25a65482fdcf
Chris Haas <chris@vendiadvertising.com>
2019-02-09 17:43:27 -0600

    Add scanner and scanner type maker

---
+++ src/Command/MakeScannerCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Client;
+use App\Entity\Scanner;
+use App\Entity\ScannerType;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class MakeScannerCommand extends AppCommandBase
+{
+    protected static $defaultName = 'app:make:scanner';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('scanner_name', InputArgument::OPTIONAL, 'Scanner name')
+            ->addArgument('scanner_type', InputArgument::OPTIONAL, 'Scanner type')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $question = (new Question('What is the name of the scanner?'))
+                            ->setValidator(
+                                function ($value) {
+
+                                    foreach($this->get_all_scanners() as $t){
+                                        if( mb_strtolower($t->getName()) === mb_strtolower($value)){
+                                            throw new \RuntimeException('A scanner with that name already exists');
+                                        }
+                                    }
+
+                                    return $value;
+                                }
+                            )
+        ;
+        $scanner_name = $this->get_arg_or_ask('scanner_name', $question);
+
+
+        $question = new ChoiceQuestion( 'What type of scanner is this?', $this->get_all_scanner_type_names());
+        $scanner_type_name = $this->get_arg_or_ask('scanner_type', $question);
+
+        $scanner_type = $this->get_scanner_type_by_name($scanner_type_name);
+        if (!$scanner_type) {
+            $io = new SymfonyStyle($this->input, $this->output);
+            $io->caution('Could not find the supplied scanner type');
+            return;
+        }
+
+        $scanner = new Scanner();
+        $scanner->setName($scanner_name);
+        $scanner->setScannerType($scanner_type);
+
+        $this->entityManager->persist($scanner);
+        $this->entityManager->flush();
+
+        $io = new SymfonyStyle($input, $output);
+        $io->success("Scanner {$scanner_name} created");
+    }
+}
