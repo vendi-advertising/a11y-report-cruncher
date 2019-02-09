ef3e393a0fe4879223b24533a418c6a680eed9dd
Chris Haas <chris@vendiadvertising.com>
2019-02-09 17:43:01 -0600

    Introduce base command for shared lookup stuff

---
+++ src/Command/AppCommandBase.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Client;
+use App\Entity\Scanner;
+use App\Entity\ScannerType;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+abstract class AppCommandBase extends Command
+{
+
+    protected $input;
+
+    protected $output;
+
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function get_all_names_of_things(array $things) : array
+    {
+        $names = [];
+        array_walk(
+            $things,
+            function ($thing) use (&$names) {
+                $names[] = $thing->getName();
+            }
+        )
+        ;
+
+        return $names;
+    }
+
+    protected function get_all_scanner_types() : array
+    {
+        return $this
+                ->entityManager
+                ->getRepository(ScannerType::class)
+                ->findAll()
+        ;
+    }
+
+    protected function get_all_scanner_type_names() : array
+    {
+        return $this->get_all_names_of_things($this->get_all_scanner_types());
+    }
+
+    protected function get_all_scanner_names() : array
+    {
+        return $this->get_all_names_of_things($this->get_all_scanners());
+    }
+
+    protected function get_scanner_type_by_name(string $name) : ?ScannerType
+    {
+        return $this
+                    ->entityManager
+                    ->getRepository(ScannerType::class)
+                    ->findOneBy(
+                        [
+                            'name' => $name,
+                        ]
+                    )
+        ;
+    }
+
+    protected function get_all_scanners() : array
+    {
+        return $this
+                ->entityManager
+                ->getRepository(Scanner::class)
+                ->findAll()
+        ;
+    }
+
+    protected function get_user_by_email(string $email) : ?User
+    {
+        return $this
+                    ->entityManager
+                    ->getRepository(User::class)
+                    ->findOneBy(
+                        [
+                            'email' => $email,
+                        ]
+                    )
+        ;
+    }
+
+    protected function get_client_by_name(string $name) : ?Client
+    {
+        return $this
+                    ->entityManager
+                    ->getRepository(Client::class)
+                    ->findOneBy(
+                        [
+                            'name' => $name,
+                        ]
+                    )
+        ;
+    }
+
+    protected function get_all_client_names() : array
+    {
+        $clients =  $this
+                     ->entityManager
+                        ->getRepository(Client::class)
+                        ->findAll()
+        ;
+
+        return $this->get_all_names_of_things($clients);
+    }
+
+    protected function get_arg_or_ask(string $arg_name, $question)
+    {
+        $value = $this->input->getArgument($arg_name);
+
+        if (!$value) {
+            //If we didn't pass one in, prompt here
+            $value = $this->ask_question($question);
+
+            if (!$value) {
+                $io = new SymfonyStyle($this->input, $this->output);
+                $io->note("No {$arg_name} supplied... exiting");
+                return;
+            }
+        }
+
+        return $value;
+    }
+
+    protected function ask_question($question)
+    {
+        if(is_string($question)){
+            $question = new Question($question);
+        }
+        return $this->getHelper('question')->ask($this->input, $this->output, $question);
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        //We need these stored for the helpers
+        $this->input = $input;
+        $this->output = $output;
+    }
+}
