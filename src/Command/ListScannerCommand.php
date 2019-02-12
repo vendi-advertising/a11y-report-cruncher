7e5dcc12ed6e2fbd12989affea79f9ff7e5b72df
cjhaas <chris@vendiadvertising.com>
2019-02-12 16:40:22 -0600

    Added list scanner command

---
+++ src/Command/ListScannerCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ListScannerCommand extends AppCommandBase
+{
+    protected static $defaultName = 'app:list:scanner';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('List all scanners')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $scanners = $this->get_all_scanners();
+
+        if(0 === count($scanners)){
+            $io = new SymfonyStyle($input, $output);
+            $io->error('No scanners found');
+            return;
+        }
+
+        $data = [];
+        foreach($scanners as $scanner){
+            $data[] = [
+                $scanner->getId(),
+                $scanner->getName(),
+                $scanner->getScannerType(),
+                $scanner->getToken(),
+            ];
+        }
+
+        (new Table($this->output))
+            ->setHeaders(['Id', 'Name', 'Type', 'Token',])
+            ->setRows($data)
+            ->render()
+        ;
+
+    }
+}
