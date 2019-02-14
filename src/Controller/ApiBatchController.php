41130c47c177d8bad714f7dbd2a3aaa1330787e4
Chris Haas <chris@vendiadvertising.com>
2019-02-14 05:59:46 -0600

    Dumping POSTs to disk for debugging

--- src/Controller/ApiBatchController.php
+++ src/Controller/ApiBatchController.php
@@ -11,15 +11,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Webmozart\PathUtil\Path;
 
 class ApiBatchController extends AbstractController
 {
     /**
      * @Route("/api/v1/batch/submit", name="api_batch_submit", methods={"GET", "POST"},)
      */
-    public function get_report_from_spider(EntityManagerInterface $entityManager, Request $request, TokenStorageInterface $tokenStorage)
+    public function get_report_from_spider(EntityManagerInterface $entityManager, Request $request, TokenStorageInterface $tokenStorage, KernelInterface $kernel, Filesystem $fileSystem)
     {
+        $scanner = $tokenStorage->getToken()->getUser();
+
+        if (!$scanner) {
+            throw new \Exception('Scanner not found');
+        }
+
+        $root_dir = $kernel->getProjectDir();
+        $dump_folder = Path::join($root_dir, 'var', 'post_dumps');
+        $fileSystem->mkdir($dump_folder);
+        $dump_file = Path::join($dump_folder, 'dump_' . date('m-d-Y_hia') . '.txt');
+
+        // if(!is_dir()
         $body = $request->getContent();
+        $fileSystem->dumpFile($dump_file, $body);
+        die;
+
         $body = '[{"url":"https://vendiadvertising.com/","contentType":"text/html; charset=UTF-8","statusCode":200,"error":null,"propertyScanUrlId":1,"subUrlRequestStatus":{"error":null,"urls":["https://vendiadvertising.com/what","https://vendiadvertising.com/work","https://vendiadvertising.com/who","https://vendiadvertising.com/insight","https://vendiadvertising.com/vendi-share","https://vendiadvertising.com/contact","https://vendiadvertising.com","https://vendiadvertising.com/portfolio-item/wiscontext","https://vendiadvertising.com/what/brand","https://vendiadvertising.com/what/design","https://vendiadvertising.com/what/digital","https://vendiadvertising.com/what/marketing","https://vendiadvertising.com/what/media","https://vendiadvertising.com/what/research","https://vendiadvertising.com/what/strategy","https://vendiadvertising.com/what/video-photography","https://vendiadvertising.com/what/web-mobile","https://vendiadvertising.com/reel","https://vendiadvertising.com/national-awards","https://vendiadvertising.com/careers","https://vendiadvertising.com/vendi-launch","https://vendiadvertising.com/work/consumer-goods","https://vendiadvertising.com/work/education","https://vendiadvertising.com/work/financial-insurance","https://vendiadvertising.com/work/healthcare","https://vendiadvertising.com/work/manufacturing"]}}]';
         $parsed = json_decode($body);
         if (!is_array($parsed)) {
@@ -29,8 +47,6 @@ class ApiBatchController extends AbstractController
         if (0 === count($parsed)) {
             throw new \Exception('Empty array');
         }
-
-        $scanner = $tokenStorage->getToken()->getUser();
 
 
         foreach ($parsed as $obj) {
