8c20f4bc39df0979e7d30dcb6dfee6779f232e08
Chris Haas <chris@vendiadvertising.com>
2019-02-14 12:39:43 -0600

    Got API endpoint for URL crawling mostly done

--- src/Controller/ApiBatchController.php
+++ src/Controller/ApiBatchController.php
@@ -7,6 +7,7 @@ use App\Entity\ScanUrl;
 use App\Repository\ScanUrlRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,77 +18,119 @@ use Webmozart\PathUtil\Path;
 
 class ApiBatchController extends AbstractController
 {
+    protected function debug_dump_post(Request $request, KernelInterface $kernel, Filesystem $fileSystem)
+    {
+        $root_dir = $kernel->getProjectDir();
+        $dump_folder = Path::join($root_dir, 'var', 'post_dumps');
+        $fileSystem->mkdir($dump_folder);
+        $dump_file = Path::join($dump_folder, 'dump_' . date('m-d-Y_hia') . '.' . time() . '.json');
+
+        $body = $request->getContent();
+        $fileSystem->dumpFile($dump_file, $body);
+    }
+
     /**
      * @Route("/api/v1/batch/submit", name="api_batch_submit", methods={"GET", "POST"},)
      */
-    public function get_report_from_spider(EntityManagerInterface $entityManager, Request $request, TokenStorageInterface $tokenStorage, KernelInterface $kernel, Filesystem $fileSystem)
+    public function get_report_from_spider(EntityManagerInterface $entityManager, Request $request, TokenStorageInterface $tokenStorage, KernelInterface $kernel, Filesystem $fileSystem, ScanUrlRepository $scanUrlRepository)
     {
         $scanner = $tokenStorage->getToken()->getUser();
 
         if (!$scanner) {
-            throw new \Exception('Scanner not found');
+            return JsonResponse::create(
+                [
+                    'error' => 'Scanner not found',
+                ],
+                Response::HTTP_UNAUTHORIZED 
+            );
         }
 
-        $root_dir = $kernel->getProjectDir();
-        $dump_folder = Path::join($root_dir, 'var', 'post_dumps');
-        $fileSystem->mkdir($dump_folder);
-        $dump_file = Path::join($dump_folder, 'dump_' . date('m-d-Y_hia') . '.txt');
+        //Turn this on to read from a specific file in the dump folder
+        $debug_mode = false;
 
-        // if(!is_dir()
-        $body = $request->getContent();
-        $fileSystem->dumpFile($dump_file, $body);
-        die;
-
-        $body = '[{"url":"https://vendiadvertising.com/","contentType":"text/html; charset=UTF-8","statusCode":200,"error":null,"propertyScanUrlId":1,"subUrlRequestStatus":{"error":null,"urls":["https://vendiadvertising.com/what","https://vendiadvertising.com/work","https://vendiadvertising.com/who","https://vendiadvertising.com/insight","https://vendiadvertising.com/vendi-share","https://vendiadvertising.com/contact","https://vendiadvertising.com","https://vendiadvertising.com/portfolio-item/wiscontext","https://vendiadvertising.com/what/brand","https://vendiadvertising.com/what/design","https://vendiadvertising.com/what/digital","https://vendiadvertising.com/what/marketing","https://vendiadvertising.com/what/media","https://vendiadvertising.com/what/research","https://vendiadvertising.com/what/strategy","https://vendiadvertising.com/what/video-photography","https://vendiadvertising.com/what/web-mobile","https://vendiadvertising.com/reel","https://vendiadvertising.com/national-awards","https://vendiadvertising.com/careers","https://vendiadvertising.com/vendi-launch","https://vendiadvertising.com/work/consumer-goods","https://vendiadvertising.com/work/education","https://vendiadvertising.com/work/financial-insurance","https://vendiadvertising.com/work/healthcare","https://vendiadvertising.com/work/manufacturing"]}}]';
+        if($debug_mode){
+            $root_dir = $kernel->getProjectDir();
+            $dump_folder = Path::join($root_dir, 'var', 'post_dumps');
+            $dump_file = Path::join($dump_folder, 'dump_02-14-2019_0623pm.1550168615.json');
+            $body = file_get_contents($dump_file);
+        }else{
+            //If we're in debug mode, we're probably using the browser so we don't want to log it
+            $this->debug_dump_post($request, $kernel, $fileSystem);
+            $body = $request->getContent();
+        }
+                
         $parsed = json_decode($body);
+
         if (!is_array($parsed)) {
-            throw new \Exception('Non array');
+            return JsonResponse::create(
+                [
+                    'error' => 'Non array',
+                ],
+                Response::HTTP_BAD_REQUEST 
+            );
         }
 
         if (0 === count($parsed)) {
-            throw new \Exception('Empty array');
+            return JsonResponse::create(
+                [
+                    'error' => 'Empty array',
+                ],
+                Response::HTTP_EXPECTATION_FAILED  
+            );
         }
 
+        foreach($parsed as $scanUrlJson){
 
-        foreach ($parsed as $obj) {
-            $propertyScanUrl = $this
-                                ->getDoctrine()
-                                ->getRepository(PropertyScanUrl::class)
-                                ->find((int) $obj->propertyScanUrlId)
-                            ;
-
-
-            $log = (new PropertyScanUrlLog())
-                    ->setDirectionIn()
-                    ->setScanner($scanner)
-                    ->setPropertyScanUrl($propertyScanUrl)
-                    ->setContentType($obj->contentType)
-            ;
-
-            if (!$obj->error) {
-                $log->setStatusSuccess();
-            } else {
-                $log->setStatusError();
-                $log->setInfo($obj->error);
+            $scanUrl = $scanUrlRepository->find($scanUrlJson->scanUrlId);
+            if(!$scanUrl){
+                return JsonResponse::create(
+                    [
+                        'error' => sprintf( 'Could not find Scan Url with id [%1$s]', $scanUrlJson->scanUrlId),
+                    ],
+                    Response::HTTP_EXPECTATION_FAILED  
+                );
             }
+            $scanUrl->setContentType($scanUrlJson->contentType);
+            $scanUrl->setHttpStatus($scanUrlJson->statusCode);
+            $scanUrl->setByteSize($scanUrlJson->byteSize);
 
-            $entityManager->persist($log);
-            /*
-                    "url": "https://vendiadvertising.com/",
-                    "contentType": "text/html; charset=UTF-8",
-                    "statusCode": 200,
-                    "error": null,
-                    "propertyScanUrlId": 1,
-                    "subUrlRequestStatus": {
-                        "error": null,
-                        "urls": [
-                        ]
+            //TODO: This error needs to be logged somewhere
+            if($scanUrlJson->error){
+                $scanUrl->setScanStatus(ScanUrl::SCAN_STATUS_ERROR);
+            }else{
+                $scanUrl->setScanStatus(ScanUrl::SCAN_STATUS_SUCCESS);
+            }
+            
+            $entityManager->persist($scanUrl);
+            
+            if(!$scanUrlJson->error){
+                if($scanUrlJson->subUrlRequestStatus){
+                    $discovered_urls = $scanUrlJson->subUrlRequestStatus->urls;
+                    foreach($discovered_urls as $disc_url){
+                        $existing = $scanUrlRepository->findBy(
+                            [
+                                'url' => $disc_url,
+                            ]
+                        );
+    
+                        if($existing){
+                            continue;
+                        }
+    
+                        $newScanUrl = new ScanUrl();
+                        $newScanUrl->setScan($scanUrl->getScan());
+                        $newScanUrl->setUrl($disc_url);
+                        $entityManager->persist($newScanUrl);
+    
                     }
-             */
+                }
+            }
         }
 
         $entityManager->flush();
 
+
+        //TODO: Return something better
         return new JsonResponse('');
 
         // file_put_contents('/var/www/a11y-primary-site/stage/wp-site/a11y-report-cruncher/tmp/data.txt', $data);
@@ -102,7 +145,12 @@ class ApiBatchController extends AbstractController
         $scanner = $tokenStorage->getToken()->getUser();
 
         if (!$scanner) {
-            throw new \Exception('No spider scanners registered');
+            return JsonResponse::create(
+                [
+                    'error' => 'Scanner not found',
+                ],
+                Response::HTTP_UNAUTHORIZED 
+            );
         }
 
         $urls = $scanUrlRepository
@@ -124,7 +172,7 @@ class ApiBatchController extends AbstractController
         foreach ($subset as $url) {
             $data['urls'][] = [
                 'url' => $url->getUrl(),
-                'id' => $url->getId(),
+                'scanUrlId' => $url->getId(),
             ];
         }
 
