893949ddf822364f08f66ae2738fb37f3f5c87ce
Chris Haas <chris@vendiadvertising.com>
2019-02-14 12:59:19 -0600

    Dump scanners to home screen

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\Scanner;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,12 +28,18 @@ class HomeController extends AbstractController
                         ->findAll()
         ;
 
+        $scanners = $entityManager
+                        ->getRepository(Scanner::class)
+                        ->findAll()
+        ;
+
         return $this
                 ->render(
                     'home/index.html.twig',
                     [
                         'properties' => $properties,
                         'users' => $users,
+                        'scanners' => $scanners,
                     ]
                 );
     }
